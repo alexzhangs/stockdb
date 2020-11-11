26ba9f53ea95979bc77c2725adf7e75bccff2223
Alex <alexzhangs@gmail.com>
2020-11-11 16:12:38 +0800

    update index/models.py

--- stockdb/index/models.py
+++ stockdb/index/models.py
@@ -7,10 +7,21 @@ from stock.models import *
 
 # Create your models here.
 
+# refer to:
+#   * https://zh.wikipedia.org/wiki/上海证券交易所综合股价指数
+#   * https://zh.wikipedia.org/wiki/深圳证券交易所成份股价指数
+#   * https://zh.wikipedia.org/wiki/恒生指數
+#   * https://zh.wikipedia.org/wiki/道琼斯工业平均指数
+#   * https://zh.wikipedia.org/wiki/納斯達克綜合指數
+#   * https://zh.wikipedia.org/wiki/富時集團
+#   * https://www.ftserussell.com/index
+#   * https://zh.wikipedia.org/wiki/美元指数
 class Index(models.Model):
-    code = models.CharField(max_length=16)
+    code = models.CharField(max_length=16, unique=True)
+    inner_code = models.CharField(max_length=16, null=True, blank=True)
     name = models.CharField(max_length=32)
     formula = models.CharField(max_length=512, null=True, blank=True)
+    region = models.ForeignKey(Region, on_delete=models.RESTRICT, related_name='indexes')
     market = models.ForeignKey(Market, to_field='code', null=True, blank=True, on_delete=models.RESTRICT, related_name='indexes')
     subject = models.ForeignKey(Subject, to_field='code', null=True, blank=True, on_delete=models.RESTRICT, related_name='indexes')
     dt_created = models.DateTimeField('Created', auto_now_add=True)
