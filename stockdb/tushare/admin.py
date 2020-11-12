3103395e13ced082208e77d6012c129a50f8b0c1
Alex <alexzhangs@gmail.com>
2020-11-12 23:06:34 +0800

    update app tushare

--- stockdb/tushare/admin.py
+++ stockdb/tushare/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+#from django.forms import ModelForm, PasswordInput
+
+from .models import *
+
 
 # Register your models here.
+
+'''
+class AccountForm(ModelForm):
+    class Meta:
+        model = Account
+        fields = ['password', 'token']
+        widgets = {
+            'password': PasswordInput(),
+            'token': PasswordInput(),
+        }
+'''
+
+@admin.register(Config)
+class ConfigAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Config._meta.local_fields]
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Account._meta.local_fields if f.name not in ['password','token']]
+    #form = AccountForm
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Category._meta.local_fields if f.name not in ['desc']]
+
+
+@admin.register(Api)
+class ApiAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Api._meta.local_fields if f.name not in ['desc']]
--- stockdb/tushare/models.py
+++ stockdb/tushare/models.py
@@ -3,5 +3,54 @@ from django.db import models
 
 # Create your models here.
 
+class Config(models.Model):
+    code = models.CharField(max_length=32, unique=True)
+    name = models.CharField(max_length=32)
+    value = models.CharField(max_length=256, null=True, blank=True)
+    dt_created = models.DateTimeField('Created', auto_now_add=True)
+    dt_updated = models.DateTimeField('Updated', auto_now=True)
+
+    def __str__(self):
+        return '%s (%s)' % (self.name, self.code)
+
+
+class Account(models.Model):
+    user_id = models.CharField(max_length=16)
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=32, null=True, blank=True)
+    token = models.CharField(max_length=64, null=True, blank=True)
+    credit = models.IntegerField(null=True, blank=True)
+    dt_created = models.DateTimeField('Created', auto_now_add=True)
+    dt_updated = models.DateTimeField('Updated', auto_now=True)
+
+    def __str__(self):
+        return '%s (%s)' % (self.username, self.user_id)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+    level = models.SmallIntegerField(help_text='Started with level 1.')
+    parent = models.ForeignKey('Category', on_delete=models.DO_NOTHING, null=True, blank=True, related_name='subs')
+    desc = models.CharField('Description', max_length=32, null=True, blank=True)
+    dt_created = models.DateTimeField('Created', auto_now_add=True)
+    dt_updated = models.DateTimeField('Updated', auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Api(models.Model):
+    code = models.CharField(max_length=32)
+    name = models.CharField(max_length=32)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, null=True, blank=True, related_name='apis')
+    desc = models.CharField('Description', max_length=32, null=True, blank=True)
+    credit = models.IntegerField(default=0)
+    dt_created = models.DateTimeField('Created', auto_now_add=True)
+    dt_updated = models.DateTimeField('Updated', auto_now=True)
+
+    def __str__(self):
+        return '%s (%s)' % (self.name, self.code)
+
+
 class Tushare():
     pass
