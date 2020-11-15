7f8092316dcea5c4b8b49328fef3e89a11b39d09
Alex <alexzhangs@gmail.com>
2020-11-15 23:17:40 +0800

    update to AutoField for the PK

--- stockdb/common/models.py
+++ stockdb/common/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 # refer to: https://zh.wikipedia.org/wiki/ISO_4217
 class Currency(models.Model):
-    id = models.IntegerField(primary_key=True,
+    id = models.AutoField(primary_key=True,
         help_text='ISO 4217, see Num, https://zh.wikipedia.org/wiki/ISO_4217')
     code = models.CharField(max_length=3, unique=True,
         help_text='ISO 4217, see Code, https://zh.wikipedia.org/wiki/ISO_4217')
@@ -22,7 +22,7 @@ class Currency(models.Model):
 # Level 3, refer to: https://zh.wikipedia.org/wiki/ISO_3166-1
 # Level 4 & 5, partly refer to: https://github.com/adyliu/china_area
 class Region(models.Model):
-    id = models.IntegerField(primary_key=True,
+    id = models.AutoField(primary_key=True,
         help_text='ISO 3166-1 for level 3, see Numeric code, https://zh.wikipedia.org/wiki/ISO_3166-1')
     code = models.CharField(max_length=9, unique=True,
         help_text='ISO 3166-1 for level 3, see Alpha-2 code, https://zh.wikipedia.org/wiki/ISO_3166-1')
